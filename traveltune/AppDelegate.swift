66ee266ff89521a530db813cd3523d5469ee3bfe
hyeseong <hyeseong7848@gmail.com>
2023-09-25 23:30:16 +0900

    SPM Firebase 제거

--- traveltune/AppDelegate.swift
+++ traveltune/AppDelegate.swift
@@ -6,14 +6,14 @@
 //
 
 import UIKit
-import Firebase
+//import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        FirebaseApp.configure()
+//        FirebaseApp.configure()
         UIView.appearance().backgroundColor = .clear
         UITabBar.appearance().clipsToBounds = true
         UITabBar.appearance().layer.borderColor = UIColor.clear.cgColor
