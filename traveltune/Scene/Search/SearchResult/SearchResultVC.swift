6d676b8bc67b1d436e273995cd814ffbc79a0c36
hyeseong <hyeseong7848@gmail.com>
2023-10-03 23:06:33 +0900

    검색결과VC - 네비바에 텍스트필드 추가

--- traveltune/Scene/Search/SearchResult/SearchResultVC.swift
+++ traveltune/Scene/Search/SearchResult/SearchResultVC.swift
@@ -13,10 +13,26 @@ final class SearchResultVC: BaseViewController<SearchResultView> {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("검색어 = ", searchKeyword)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        mainView.naviBarSearchTextField.resignFirstResponder()
     }
     
     override func configureVC() {
-        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            image: .chevronBackward,
+            style: .plain,
+            target: self,
+            action: #selector(backButtonClicked)
+        )
+        navigationItem.leftBarButtonItem?.tintColor = .txtPrimary
+        mainView.naviBarSearchTextField.text = searchKeyword ?? ""
+        navigationItem.titleView = mainView.naviBarSearchTextField
+    }
+    
+    @objc private func backButtonClicked() {
+        navigationController?.popViewController(animated: true)
     }
 }
--- traveltune/Scene/Search/SearchResult/SearchResultView.swift
+++ traveltune/Scene/Search/SearchResult/SearchResultView.swift
@@ -9,11 +9,31 @@ import UIKit
 
 final class SearchResultView: BaseView {
     
+    lazy var naviBarSearchTextField = SearchTextField().setup { view in
+        let width = UIScreen.main.bounds.width - 80
+        view.frame = .init(x: 0, y: 0, width: width, height: 40)
+        view.returnKeyType = .search
+        view.addDoneOnKeyboardWithTarget(self, action: #selector(toolBarDoneClicked), titleText: nil)
+        //        view.keyboardToolbar.doneBarButton.setTarget(self, action: #selector(keyboardDoneClicked))
+        view.delegate = self
+    }
+    
+    @objc private func toolBarDoneClicked() {
+        naviBarSearchTextField.resignFirstResponder()
+    }
+    
     override func configureHierarchy() {
         
     }
     
     override func configureLayout() {
         
+    }
+}
+
+extension SearchResultView: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("키보드 검색 버튼 클릭")
+        return true
     }
 }
