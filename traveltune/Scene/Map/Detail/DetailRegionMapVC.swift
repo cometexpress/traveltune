ad54c391666e25016cb0dbd95a93f9ce0d9b0728
hyeseong <hyeseong7848@gmail.com>
2023-10-12 21:10:30 +0900

    지역 지도상세 - 내 위치로 이동기능 추가, LocationManager 클래스 추가

--- traveltune/Scene/Map/Detail/DetailRegionMapVC.swift
+++ traveltune/Scene/Map/Detail/DetailRegionMapVC.swift
@@ -12,22 +12,8 @@ final class DetailRegionMapVC: BaseViewController<DetailRegionMapView, DetailReg
     
     private var naviTitle = ""
     
-    /**
-     1. 위치 시스템 권한 확인
-     a. ON 상태, 위치 권한 요청 가능
-     i. 권한 허용
-     - 위치 데이터 접근 가능
-     - 추후 권한을 거부할 경우에 다시 iOS 시스템 에서 설정 유도
-     ii. 권한 거부
-     - iOS 시스템 설정 유도
-     b. OFF 상태, 위치 권한 요청 불가능
-     - iOS 시스템 설정 유도
-     */
-    
-    private let locationManager = CLLocationManager()
-    
     deinit {
-        locationManager.stopUpdatingLocation()
+        LocationManager.shared.stopUpdating()
     }
     
     override func viewDidLoad() {
@@ -35,86 +21,22 @@ final class DetailRegionMapVC: BaseViewController<DetailRegionMapView, DetailReg
         configureVC()
         bindViewModel()
         
-        /**
-         내 위치 버튼 눌렀을 때 권한 체크 메서드 실행
-         - 허용: 내 위치로 이동
-         - 거부: 위치 권한 설정으로 이동할. 수 있도록 Alert 띄우기
-         */
-        locationManager.requestWhenInUseAuthorization()
-        checkDeviceLocationAuthorization()
-    }
-    
-    // 아이폰 GPS 기능 - ON or OFF 체크
-    func checkDeviceLocationAuthorization() {
-        
-        DispatchQueue.global().async { [weak self] in
-            guard let self else { return }
-            // locationServicesEnabled - 메인스레드에서 체크하면 안됨.
-            // 아이폰 설정에서 위치서비스 확인 (gps설정 확인)
-            if CLLocationManager.locationServicesEnabled() {
-                let authorization = self.locationManager.authorizationStatus
-                DispatchQueue.main.async {
-                    print("authorization = \(authorization)")
-                    self.checkCurrentLocationAuthorization(status: authorization)
-                }
-
-            } else {
-                // GPS Off 상태
-                DispatchQueue.main.async {
-                    print("아이폰 위치서비스가 꺼져있으면 위치 권한요청을 할 수 없음")
-                    self.showLocationSettingAlert()
-                }
+        // 위치 권한 상태값 확인
+        LocationManager.shared.locationAuthorizationStatus.bind { [weak self] authrizationStatus in
+            switch authrizationStatus {
+            case .initAuth: Void()
+            case .none: Void()
+            case .notDetermined:
+                LocationManager.shared.requestAuthorization()
+            case .denied:
+                // iOS 설정으로 보내라는 alert 띄우기
+                self?.showLocationSettingAlert()
+            case .authorized:
+                // didUpDateLocationer
+                LocationManager.shared.startUpdating()
+                self?.mainView.mapView.setUserTrackingMode(.follow, animated: true)
             }
         }
-    }
-    
-    func checkCurrentLocationAuthorization(status: CLAuthorizationStatus) {
-        switch status {
-        case .notDetermined:
-//            print("최초로 앱을 유저가 실행했을 때")
-            print(#function, "\(status)")
-            locationManager.desiredAccuracy = kCLLocationAccuracyBest  // 위치 정확도 설정
-            locationManager.requestWhenInUseAuthorization()  // 얼럿을 띄워주는 역할 - info.plist 에 알맞은 권한 명시해줘야 얼럿 동작
-        case .restricted:
-            print("restricted")
-        case .denied:
-            print("denied")
-            // iOS 설정으로 보내라는 alert 띄우기
-            showLocationSettingAlert()
-        case .authorizedAlways:
-            print("authorizedAlways")
-        case .authorizedWhenInUse:
-            print("authorizedWhenInUse")
-            //didUpDateLocationr
-            locationManager.startUpdatingLocation()
-        case .authorized:
-            print("authorized")
-        @unknown default:   // 위치 권한 종류가 추후에 더 생길 가능성을 대비해준 것
-            print("default")
-        }
-    }
-    
-    func showLocationSettingAlert() {
-        print(UIApplication.openSettingsURLString)
-        
-        let alert = UIAlertController(
-            title: Strings.Common.locationServices,
-            message: Strings.Common.locationServicesGuide,
-            preferredStyle: .alert
-        )
-        
-        // 설정에서 직접적으로 앱 설정 화면에 들어간 적이 없다면
-        // 한번도 설정 앱에 들어가지 않았거나, 막 다운받은 앱이라서 앱 상세까지 못들어간다.
-        // 유저가 들어간 적이 있다면 앱 상세까지 자동으로 이동됨.
-        let goSetting = UIAlertAction(title: Strings.Common.move, style: .default) { _ in
-            if let appSetting = URL(string: UIApplication.openSettingsURLString) {
-                UIApplication.shared.open(appSetting)
-            }
-        }
-        let cancel = UIAlertAction(title: Strings.Common.cancel, style: .cancel)
-        alert.addAction(goSetting)
-        alert.addAction(cancel)
-        present(alert, animated: true)
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -154,12 +76,40 @@ final class DetailRegionMapVC: BaseViewController<DetailRegionMapView, DetailReg
     @objc private func backButtonClicked() {
         navigationController?.popViewController(animated: true)
     }
+    
+    // GPS, 위치권한 획득 확인
+    private func checkAvailableCurrentLocation() {
+        LocationManager.shared.requestAuthorization()
+        LocationManager.shared.deviceGpsStatus { [weak self] gps, authStatus in
+            switch gps {
+            case .on:
+                if let authStatus {
+                    LocationManager.shared.checkCurrentLocationAuthorization(status: authStatus)
+                }
+            case .off:
+                self?.showLocationSettingAlert()
+            }
+        }
+    }
+    
+    private func showLocationSettingAlert() {
+        print(UIApplication.openSettingsURLString)
+        showAlert(
+            title: Strings.Common.locationServices,
+            msg: Strings.Common.locationServicesGuide,
+            ok: Strings.Common.move,
+            no: Strings.Common.cancel) { _ in
+                if let appSetting = URL(string: UIApplication.openSettingsURLString) {
+                    UIApplication.shared.open(appSetting)
+                }
+            }
+    }
 }
 
 extension DetailRegionMapVC: DetailRegionMapVCProtocol {
     
     func currentLocationClicked() {
-        mainView.mapView.setUserTrackingMode(.follow, animated: true)
+        checkAvailableCurrentLocation()
     }
 }
 
@@ -174,7 +124,6 @@ extension DetailRegionMapVC: CLLocationManagerDelegate {
     // 한번만 실행되는 것이 아니라 위치가 계속 바뀌면 계속 호출됨 , iOS 위치 업데이트가 필요한 시점에 알아서 여러번 호출!!
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         print(#function ,locations)
-        
 //        if let coordinate = locations.last?.coordinate {
 //            print(coordinate)
 //            setRegionAndAnnotation(center: coordinate)
@@ -194,6 +143,6 @@ extension DetailRegionMapVC: CLLocationManagerDelegate {
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         // 어떤 권한인지는 모르고 권한이 바뀌는 것만 암,
         print(#function)
-        checkDeviceLocationAuthorization()
+        checkAvailableCurrentLocation()
     }
 }
--- traveltune/Utils/LocationManager.swift
+++ traveltune/Utils/LocationManager.swift
@@ -11,30 +11,80 @@ import Foundation
 final class LocationManager {
     static let shared = LocationManager()
     private init() {}
-    let locationManager = CLLocationManager()
+    
+    private var locationManager: CLLocationManager {
+        let manager = CLLocationManager()
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        return manager
+    }
     
     enum DeviceGPS {
         case on, off
     }
     
-    func deviceLoactionStatus(completion: @escaping (DeviceGPS, CLAuthorizationStatus?) -> ()) {
+    enum LocationAuthorizationStatus {
+        case initAuth, none, notDetermined, denied, authorized
+    }
+    
+    var locationAuthorizationStatus: Observable<LocationAuthorizationStatus> = Observable(.initAuth)
+        
+    func requestAuthorization() {
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    func startUpdating() {
+        locationManager.startUpdatingLocation()
+    }
+    
+    func stopUpdating() {
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func deviceGpsStatus(completion: @escaping (DeviceGPS, CLAuthorizationStatus?) -> ()) {
         DispatchQueue.global().async {
             if CLLocationManager.locationServicesEnabled() {
                 let authorization: CLAuthorizationStatus
-                
                 if #available(iOS 14.0, *) {
                     authorization = self.locationManager.authorizationStatus
                 } else {
                     authorization = CLLocationManager.authorizationStatus()
                 }
-                completion(DeviceGPS.on, authorization)
-
+                
+                DispatchQueue.main.async {
+                    completion(DeviceGPS.on, authorization)
+                }
+                
             } else {
                 print("아이폰 위치서비스가 꺼져있으면 위치 권한요청을 할 수 없음")
-                completion(DeviceGPS.off, nil)
+                DispatchQueue.main.async {
+                    completion(DeviceGPS.off, nil)
+                }
             }
         }
     }
     
-    
+    func checkCurrentLocationAuthorization(status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            print("최초로 앱을 유저가 실행했을 때")
+            locationAuthorizationStatus.value = .notDetermined
+        case .restricted:
+            print("restricted")
+            locationAuthorizationStatus.value = .none
+        case .denied:
+            print("denied")
+            locationAuthorizationStatus.value = .denied
+        case .authorizedAlways:
+            print("authorizedAlways")
+            locationAuthorizationStatus.value = .authorized
+        case .authorizedWhenInUse:
+            print("authorizedWhenInUse")
+            locationAuthorizationStatus.value = .authorized
+        case .authorized:
+            print("authorized")
+            locationAuthorizationStatus.value = .authorized
+        @unknown default:   // 위치 권한 종류가 추후에 더 생길 가능성을 대비해준 것
+            locationAuthorizationStatus.value = .none
+        }
+    }
 }
