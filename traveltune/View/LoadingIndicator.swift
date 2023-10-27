177eb8be09673976056cc25630bf8c56fed3bedc
hyeseong <hyeseong7848@gmail.com>
2023-10-27 17:11:36 +0900

    로딩인디케이터 오류 수정

--- traveltune/View/LoadingIndicator.swift
+++ traveltune/View/LoadingIndicator.swift
@@ -11,26 +11,37 @@ final class LoadingIndicator {
     
     static func show() {
         LoadingIndicator.hide()
+        
         DispatchQueue.main.async {
-//            guard let window = UIApplication.shared.windows.last else { return }
-            guard let window = UIApplication.shared.connectedScenes.map({ $0 as? UIWindowScene }).compactMap({ $0 }).first?.windows.first else { return }
-                let indicatorView: UIActivityIndicatorView
-                if let existedView = window.subviews.first(where: { $0 is UIActivityIndicatorView } ) as? UIActivityIndicatorView {
-                    indicatorView = existedView
-                } else {
-                    indicatorView = UIActivityIndicatorView(style: .large)
-                    indicatorView.frame = window.frame
-                    indicatorView.color = .primary
-                    window.addSubview(indicatorView)
-                }
-                indicatorView.startAnimating()
+            let window: UIWindow? = if #available(iOS 15.0, *) {
+                UIApplication.shared.connectedScenes.map({ $0 as? UIWindowScene }).compactMap({ $0 }).first?.windows.first
+            } else {
+                UIApplication.shared.windows.last
+           }
+            
+            guard let window else { return }
+            
+            let indicatorView: UIActivityIndicatorView
+            if let existedView = window.subviews.first(where: { $0 is UIActivityIndicatorView } ) as? UIActivityIndicatorView {
+                indicatorView = existedView
+            } else {
+                indicatorView = UIActivityIndicatorView(style: .large)
+                indicatorView.frame = window.frame
+                indicatorView.color = .primary
+                window.addSubview(indicatorView)
+            }
+            indicatorView.startAnimating()
         }
     }
     
     static func hide() {
         DispatchQueue.main.async {
-//            guard let window = UIApplication.shared.windows.last else { return }
-            guard let window = UIApplication.shared.connectedScenes.map({ $0 as? UIWindowScene }).compactMap({ $0 }).first?.windows.first else { return }
+            let window: UIWindow? = if #available(iOS 15.0, *) {
+                UIApplication.shared.connectedScenes.map({ $0 as? UIWindowScene }).compactMap({ $0 }).first?.windows.first
+            } else {
+                UIApplication.shared.windows.last
+           }
+            guard let window else { return }
             window.subviews.filter({ $0 is UIActivityIndicatorView }).forEach { $0.removeFromSuperview() }
         }
     }
